1b5bb8ea95036b93770ffec0cc68865413028daa
Renato Atilio <renatoat@gmail.com>
2018-06-17 23:47:53 -0300

    Importador de partidas 100% funcional, a unica verificacao nao feita em relacao a importacao de colecao é o ano de publicacao (na partida precisa fazer uma request adicional ao bgg)

--- README.md
+++ README.md
@@ -25,6 +25,10 @@ Devido a forma como a Ludopedia está construída atualmente, não há disponív
 
 A Ludopedia utiliza o BGG como insumo para sua base de dados, mas não possui nenhum local onde nos permita fazer uma busca utilizando o *id* do BGG. Isso quer dizer que as buscas são realizadas utilizando o *nome do jogo* e comparando se o *ano de lançamento* bate, por isso alguns jogos não serão adicionados ou pode ocorrer de serem adicionados ítens errados na coleção.
 
+### Marcando amigos na importação de partidas
+
+Não tendo uma forma automática de identificar outros usuários do BGG na Ludopedia, para marcar amigos nas partidas, uma possibilidade é criar um arquivo `usuarios.txt` neste mesmo diretório, tendo como conteúdo um usuário por linha, no formato `nome_usuario_bgg=id_usuario_ludopedia`.
+
 ### Problemas, dúvidas ou sugestões?
 
 Caso tenha qualquer tipo de dúvida, problema ou sugestão, fique a vontade em abrir uma [issue](https://github.com/dambros/importador-bgg-ludopedia/issues) ou deixar uma mensagem no [tópico oficial](https://ludopedia.com.br/topico/24305/importador-de-colecao-bgg-ludopedia) na Ludopedia que responderei o mais rápido possível.
--- importador.py
+++ importador.py
@@ -1,7 +1,10 @@
 import sys
 import time
-import datetime
+from datetime import datetime
+import re
 from xml.etree import ElementTree
+from configparser import ConfigParser
+from itertools import chain
 
 import colorama
 import requests
@@ -15,37 +18,24 @@ def start():
     print(
         "\n\n--------------***Importador BGG - Ludopedia***--------------\n\n"
         "Por favor entre abaixo as informações necessárias para importar "
-        "uma coleção do BGG para a Ludopedia\n\n")
+        "dados do BGG para a Ludopedia\n\n")
 
     bgg_user = input("Username BGG: ")
     ludopedia_email = input("Email Ludopedia: ")
     ludopedia_pass = input("Password Ludopedia: ")
+    option = input("Importar (1) Coleção  (2) Partidas [Padrão: 1]: ")
 
-    bgg_collection = get_bgg_collection(bgg_user)
-    session = login_ludopedia(ludopedia_email, ludopedia_pass)
-    import_collection(session, bgg_collection)
-
-    print(colorama.Fore.GREEN + 'Importação finalizada com sucesso!\n\n')
-    input("Pressione ENTER para sair")
-
-def start2():
-    colorama.init()
-    print(
-        "\n\n--------------***Importador BGG - Ludopedia***--------------\n\n"
-        "Por favor entre abaixo as informações necessárias para importar "
-        "uma lista de partidas do BGG para a Ludopedia\n\n")
-
-    bgg_user = input("Username BGG: ")
-    ludopedia_email = input("Email Ludopedia: ")
-    ludopedia_pass = input("Password Ludopedia: ")
-
-    bgg_plays = get_bgg_plays(bgg_user)
-    session = login_ludopedia(ludopedia_email, ludopedia_pass)
-    import_plays(session, bgg_plays)
+    if (option == '2'):
+        bgg_plays = get_bgg_plays(bgg_user)
+        (session, ludo_user_id) = login_ludopedia(ludopedia_email, ludopedia_pass)
+        import_plays(session, bgg_plays, bgg_user, ludo_user_id)
+    else:
+        bgg_collection = get_bgg_collection(bgg_user)
+        (session, ) = login_ludopedia(ludopedia_email, ludopedia_pass)
+        import_collection(session, bgg_collection)
 
     print(colorama.Fore.GREEN + 'Importação finalizada com sucesso!\n\n')
     input("Pressione ENTER para sair")
-
 
 def get_bgg_collection(username):
     print("Obtendo coleção do BGG...\n")
@@ -90,8 +80,10 @@ def login_ludopedia(email, password):
                                   'fornecidas, abortando...')
         sys.exit(0)
 
-    return session
-
+    user_re = re.search('id_usuario=(\d+)', r.text)
+    user_id = user_re.group(1) if user_re else None
+    
+    return (session, user_id)
 
 def import_collection(session, collection):
     print("Importando coleção...\n")
@@ -124,58 +116,87 @@ def import_collection(session, collection):
                     break
 
 def get_bgg_plays(username):
-    print("Obtendo partidas do BGG...\n")
-    plays_url = '{}{}'.format(BGG_API, 'plays')
-    params = {'username': username}
-    # TODO plays has pagination, loop through all pages (100 plays per page)
-
-    response = requests.get(plays_url, params=params)
-    while response.status_code == 202:
-        time.sleep(3)
-        response = requests.get(plays_url, params=params)
-
+    has_more = True
+    page = 0
     plays = []
-    if response.status_code == 200:
-        root = ElementTree.fromstring(response.content)
 
-        if root.tag == 'errors':
-            print(colorama.Fore.RED + 'Usuário BGG inválido, abortando...')
-            sys.exit(0)
-        else:
-            total_partidas = root.attrib['total']
-            print('Total de partidas encontradas no BGG: {}\n'.format(total_partidas))
+    today = datetime.today().strftime('%d/%m/%Y')
+    min_date = input(f'Partidas a partir de [dd/mm/aaaa, padrão: {today}]: ')
 
-            for play in root.findall('play'):
-                date = play.get('date')
-                length = play.get('length')
-                location = play.get('location')
+    try:
+        datetime.strptime(min_date, '%d/%m/%Y')
+    except ValueError:
+        print(f'\nData invalida, usando o padrão {today}')
+        min_date = today
 
-                gameName = play.findall('item')[0].get('name')
+    while (has_more):
+        page += 1
 
-                commentsEl = play.find('comments')
-                comments = commentsEl.text if commentsEl != None else None
+        print(f'\nObtendo partidas do BGG, página {page}\n')
+        plays_url = f'{BGG_API}plays'
+        params = {'username': username, 'page': page, 'mindate': datetime.strptime(min_date, '%d/%m/%Y').strftime('%Y-%m-%d') }
 
-                players = []
-                for player in play.find('players').findall('player'):
-                    name = player.get('name')
-                    username = player.get('username')
-                    startposition = player.get('startposition')
-                    score = player.get('score')
-                    win = player.get('win')
-                    players.append((name, username, startposition, score, win))
+        response = requests.get(plays_url, params=params)
+        while response.status_code == 202:
+            time.sleep(3)
+            response = requests.get(plays_url, params=params)
 
-                plays.append((date, length, location, gameName, comments, players))
+        if response.status_code == 200:
+            root = ElementTree.fromstring(response.content)
+
+            if root.text != None and root.text.strip() == 'Invalid object or user':
+                print(colorama.Fore.RED + 'Usuário BGG inválido, abortando...')
+                sys.exit(0)
+            else:
+                total_partidas = root.get('total')
+                print(f'Total de partidas encontradas no BGG: {total_partidas}\n')
+
+                for play in root.findall('play'):
+                    date = play.get('date')
+                    length = play.get('length')
+                    location = play.get('location')
+
+                    gameName = play.findall('item')[0].get('name')
+
+                    commentsEl = play.find('comments')
+                    comments = commentsEl.text if commentsEl != None else None
+
+                    players = []
+                    for player in play.find('players').findall('player'):
+                        name = player.get('name')
+                        bgguser = player.get('username')
+                        startposition = player.get('startposition')
+                        score = player.get('score')
+                        win = player.get('win')
+                        players.append((name, bgguser, startposition, score, win))
+                    
+                    # sort players, me first
+                    players.sort(key=lambda p: 100*(p[1] != username) + int(p[2]))
+
+                    plays.append((date, length, location, gameName, comments, players))
+
+                print(f'Total de partidas importadas: {len(plays)}\n')
+
+                if (len(plays) >= int(total_partidas)):
+                    has_more = False
 
     return plays
 
-def import_plays(session, plays):
+def import_plays(session, plays, my_bgg_user, ludo_user_id):
     print("Importando partidas...\n")
-    ludopedia_search_url = '{}{}'.format(LUDOPEDIA_URL,
-                                         'classes/ajax/aj_search.php')
+    ludopedia_search_url = f'{LUDOPEDIA_URL}classes/ajax/aj_search.php'
     params = {'tipo': 'jogo', 'count': 'true', 'pagina': 1, 'qt_rows': 20}
 
-    ludopedia_add_play_url = '{}{}'.format(LUDOPEDIA_URL,
-                                           'cadastra_partida')
+    ludopedia_add_play_url = f'{LUDOPEDIA_URL}cadastra_partida'
+
+    try:
+        parser = ConfigParser()
+        with open("usuarios.txt") as lines:
+            lines = chain(("[top]",), lines)
+            parser.read_file(lines)
+            ludo_users = dict(parser['top'])
+    except FileNotFoundError:
+        ludo_users = {}
 
     for bgg_play in plays:
         (date, length, location, gameName, comments, players) = bgg_play
@@ -191,27 +212,25 @@ def import_plays(session, plays):
 
                 if 1==1:#item['ano_publicacao'] == year_published:
                     id_jogo = item['id_jogo']
-                    # TODO get MY_ID automatically
-                    MY_ID = 52973
 
                     payload_add_play = {
                         'id_jogo': id_jogo,
-                        'dt_partida': datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%d/%m/%y'),
+                        'dt_partida': datetime.strptime(date, '%Y-%m-%d').strftime('%d/%m/%Y'),
                         'qt_partidas': 1,
                         'duracao_h': int(int(length)/60),
                         'duracao_m': int(length)%60,
                         'descricao': comments,
 
-                        # (name, username, startposition, score, win)
-                        # TODO not very clear what id_partida_jogador does
-                        'id_partida_jogador[]': map(lambda p: 0 if p[1] == "renatoat" else '', players),
-                        'id_usuario[]': map(lambda p: MY_ID if p[1] == "renatoat" else None, players),
+                        # (name, bgguser, startposition, score, win)
+                        'id_partida_jogador[]': map(lambda p: 0, players),
+                        'id_usuario[]': map(lambda p: ludo_user_id if my_bgg_user == p[1] else ludo_users.get(p[1]), players),
                         'nome[]': map(lambda p: p[0], players),
                         'fl_vencedor[]': map(lambda p: p[4], players),
                         'vl_pontos[]': map(lambda p: p[3], players),
-                        'observacao[]': map(lambda p: 'Jogador {}{}{}'.format(p[2], ', usuário BGG ' if p[1] else '', p[1]), players)
+                        'observacao[]': map(lambda p: f'Jogador {p[2]}', players)
                     }
+                    session.post(ludopedia_add_play_url, data=payload_add_play)
                     break
 
 if __name__ == "__main__":
-    start2()
+    start()
