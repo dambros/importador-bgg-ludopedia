876e028d57207ee5d94c7bef039dfa31679e41d7
Tulio Leao <tupaschoal@gmail.com>
2021-01-09 10:55:44 -0300

    Improve play extraction2 (#9)
    * Fix get_bgg_plays increasing page when request failed
    
    * Improve progress display on bgg plays importing
    
    * Extract function to get players from a play
    
    * Use named tuples for Player information
    
    * Case insensitive comparison for bgg username
    
    Usernames can have uppercases, but for the database they are treated all in a case insensitive manner
    
    * Extract method to parse a play returned by BGG
    
    * Use named tuples for Play information
    
    * Extract function to map bgg<->ludopedia users
    
    * Get color from each bgg player
    
    * Ignore usuarios.txt file
    
    To prevent devs from inadvertently pushing it

--- importador.py
+++ importador.py
@@ -5,17 +5,38 @@ Script to import data from BoardGameGeek into Ludopedia
 import sys
 import time
 from datetime import datetime
+from math import ceil
 import re
 from xml.etree import ElementTree
 from configparser import ConfigParser
 from itertools import chain
+from typing import List, NamedTuple
 
 import colorama
 import requests
 
 BGG_API = 'https://www.boardgamegeek.com/xmlapi2/'
 LUDOPEDIA_URL = 'https://www.ludopedia.com.br/'
+BGG_PLAYS_PER_PAGE = int(100)
 
+class Player(NamedTuple):
+    """Represents a player in a BGG logged play"""
+    name: str
+    bgg_user: str
+    start_position: str
+    color: str
+    score: str
+    win: bool
+
+class Play(NamedTuple):
+    """Represents a logged BGG play"""
+    date: str
+    length: int
+    location: str
+    game_name: str
+    year_published: int
+    comments: str
+    players: List[Player]
 
 def start():
     """ Process input from user and import accordingly """
@@ -148,19 +169,50 @@ def get_date_from_user(text, default_date):
         date = default_date
     return date
 
+def get_players_from_play(play):
+    """Returns a list of players that took part in a game"""
+    players = []
+    for player in play.find('players').findall('player'):
+        players.append(Player(
+            name=player.get('name'),
+            bgg_user=player.get('username'),
+            start_position=player.get('startposition'),
+            color=player.get('color'),
+            score=player.get('score'),
+            win=player.get('win')
+        ))
+    return players
+
+def parse_play(play, username):
+    """Given an BGG xml play, return a tuple with relevant play data"""
+    game = play.findall('item')[0]
+    comments_element = play.find('comments')
+    players = get_players_from_play(play)
+
+    # sort players, me first
+    players.sort(key=lambda p: (p[1] != username, p[2]))
+
+    return Play(
+        date=play.get('date'),
+        length=play.get('length'),
+        location=play.get('location'),
+        game_name=game.get('name'),
+        year_published=get_yearpublished_from_id(game.get('objectid')),
+        comments=comments_element.text if comments_element is not None else None,
+        players=players,
+    )
+
 def get_bgg_plays(username):
     """Get all logged plays from a BGG user"""
-    has_more = True
-    page = 0
-    plays = []
 
     min_date = get_date_from_user(f'a partir de', datetime.today().strftime('%d/%m/%Y'))
     max_date = get_date_from_user(f'até', min_date)
 
+    has_more = True
+    page = 1
+    total_pages = 1
+    plays = []
     while has_more:
-        page += 1
-
-        print(f'\nObtendo partidas do BGG, página {page}\n')
         plays_url = f'{BGG_API}plays'
         params = {
             'username': username,
@@ -178,42 +230,34 @@ def get_bgg_plays(username):
                 print(f'{colorama.Fore.RED}Usuário BGG inválido, abortando...')
                 sys.exit(0)
             else:
-                total_partidas = root.get('total')
-                print(f'Total de partidas encontradas no BGG: {total_partidas}\n')
+                if page == 1:
+                    total_partidas = root.get('total')
+                    total_pages = ceil(int(total_partidas)/BGG_PLAYS_PER_PAGE)
+                    print(f'Total de partidas encontradas no BGG: {total_partidas}\n')
 
-                for play in root.findall('play'):
-                    date = play.get('date')
-                    length = play.get('length')
-                    location = play.get('location')
+                print(f'Obtendo partidas do BGG, página {page}/{total_pages}')
 
-                    game = play.findall('item')[0]
-                    game_name = game.get('name')
-                    year_published = get_yearpublished_from_id(game.get('objectid'))
-
-                    comments_element = play.find('comments')
-                    comments = comments_element.text if comments_element is not None else None
-
-                    players = []
-                    for player in play.find('players').findall('player'):
-                        name = player.get('name')
-                        bgguser = player.get('username')
-                        startposition = player.get('startposition')
-                        score = player.get('score')
-                        win = player.get('win')
-                        players.append((name, bgguser, startposition, score, win))
-
-                    # sort players, me first
-                    players.sort(key=lambda p: (p[1] != username, p[2]))
-
-                    plays.append((date, length, location, game_name,
-                                  year_published, comments, players))
+                plays.extend(parse_play(play, username) for play in root.findall('play'))
 
                 print(f'Total de partidas importadas: {len(plays)}\n')
 
                 if len(plays) >= int(total_partidas):
                     has_more = False
+                else:
+                    page += 1
 
     return plays
+
+def get_bgg_to_ludo_users():
+    """Reads usuarios.txt file to map a bgg user to its corresponding ludopedia one"""
+    try:
+        parser = ConfigParser()
+        with open("usuarios.txt") as lines:
+            lines = chain(("[top]",), lines)
+            parser.read_file(lines)
+            return dict(parser['top'])
+    except FileNotFoundError:
+        return {}
 
 def import_plays(session, plays, my_bgg_user, ludo_user_id):
     """Import all logged plays into Ludopedia"""
@@ -222,21 +266,10 @@ def import_plays(session, plays, my_bgg_user, ludo_user_id):
     params = {'tipo': 'jogo', 'count': 'true', 'pagina': 1, 'qt_rows': 20}
 
     ludopedia_add_play_url = f'{LUDOPEDIA_URL}cadastra_partida'
-
-    try:
-        parser = ConfigParser()
-        with open("usuarios.txt") as lines:
-            lines = chain(("[top]",), lines)
-            parser.read_file(lines)
-            ludo_users = dict(parser['top'])
-    except FileNotFoundError:
-        ludo_users = {}
+    ludo_users = get_bgg_to_ludo_users()
 
     for bgg_play in plays:
-        # Location is not available in Ludopedia so it is not used here
-        (date, length, _, game_name, year_published, comments, players) = bgg_play
-
-        params['nm_jogo'] = game_name
+        params['nm_jogo'] = bgg_play.game_name
         game_request = session.get(ludopedia_search_url, params=params)
         data = game_request.json()['data']
 
@@ -244,12 +277,13 @@ def import_plays(session, plays, my_bgg_user, ludo_user_id):
             found = None
 
             for item in data:
-                if item['ano_publicacao'] == year_published:
+                if item['ano_publicacao'] == bgg_play.year_published:
                     found = item
                     break
 
             if not found:
-                print(f'Nenhum jogo encontrado no ano de lançamento: {game_name} {year_published}')
+                print(f'Nenhum jogo encontrado no ano de lançamento:'
+                      f' {bgg_play.game_name} {bgg_play.year_published}')
 
                 found = data[0]
                 print(f"Importando o primeiro resultado: "
@@ -259,24 +293,24 @@ def import_plays(session, plays, my_bgg_user, ludo_user_id):
 
             payload_add_play = {
                 'id_jogo': id_jogo,
-                'dt_partida': datetime.strptime(date, '%Y-%m-%d').strftime('%d/%m/%Y'),
+                'dt_partida': datetime.strptime(bgg_play.date, '%Y-%m-%d').strftime('%d/%m/%Y'),
                 'qt_partidas': 1,
-                'duracao_h': int(int(length)/60),
-                'duracao_m': int(length)%60,
-                'descricao': comments,
+                'duracao_h': int(int(bgg_play.length)/60),
+                'duracao_m': int(bgg_play.length)%60,
+                'descricao': bgg_play.comments,
 
                 # (name, bgguser, startposition, score, win)
-                'id_partida_jogador[]': map(lambda p: 0 if my_bgg_user == p[1] else '', players),
-                'id_usuario[]': map(lambda p: ludo_user_id if my_bgg_user == p[1] else ludo_users.get(p[1], ''), players),
-                'nome[]': map(lambda p: p[0], players),
-                'fl_vencedor[]': map(lambda p: p[4], players),
-                'vl_pontos[]': map(lambda p: p[3], players),
-                'observacao[]': map(lambda p: f'Jogador {p[2]}', players)
+                'id_partida_jogador[]': map(lambda p: 0 if my_bgg_user.lower() == p.bgg_user.lower() else '', bgg_play.players),
+                'id_usuario[]': map(lambda p: ludo_user_id if my_bgg_user.lower() == p.bgg_user.lower() else ludo_users.get(p.bgg_user, ''), bgg_play.players),
+                'nome[]': map(lambda p: p.name, bgg_play.players),
+                'fl_vencedor[]': map(lambda p: p.win, bgg_play.players),
+                'vl_pontos[]': map(lambda p: p.score, bgg_play.players),
+                'observacao[]': map(lambda p: f'Jogador {p.start_position} - Cor {p.color}', bgg_play.players)
             }
             session.post(ludopedia_add_play_url, data=payload_add_play)
 
         else:
-            print(f'Jogo não encontrado na Ludopedia: {game_name}')
+            print(f'Jogo não encontrado na Ludopedia: {bgg_play.game_name}')
 
 if __name__ == "__main__":
     start()
